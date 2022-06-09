12ee1dc27c4205c71c01ca7aeace2e441454f98c
hengm3467 <100685635+hengm3467@users.noreply.github.com>
2022-06-09 16:04:38 +0800

    updated sidebar ids

--- sidebars.js
+++ sidebars.js
@@ -92,7 +92,7 @@ const sidebars = {
           type: 'ref',
           label: 'CREATE SOURCE',
           id: 'sql/commands/create-source',
-          }, 'sql-create-table', 'sql-select'
+          }, 'sql/sql-create-table', 'sql/sql-select'
         ]
         },
       ],
