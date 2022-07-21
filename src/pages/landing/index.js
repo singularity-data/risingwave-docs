314e169623d752bb7d55cc810aa5861914b68cae
mikechesterwang <mikechesterwang@foxmail.com>
2022-07-21 17:24:09 +0800

    chore: use correct font for risingwave

--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -2,8 +2,11 @@ import React, { useEffect } from "react";
 import Layout from "@theme/Layout";
 import Link from "@docusaurus/Link";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
+import ThemedImage from "@theme/ThemedImage";
 import HomePageWave from "@site/src/components/HomePageWave";
 import LogoSvg from "@site/static/img/logo.svg";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import { useColorMode } from '@docusaurus/theme-common'
 import HomepageFeature from "@site/src/components/HomepageFeature";
 import HomepageBanner from "@site/src/components/HomepageBanner";
 import useWindowSize from "@site/src/hooks/useWindowSize";
@@ -11,6 +14,8 @@ import { Button } from "@mui/material";
 import styled from "@emotion/styled";
 import Translate from "@docusaurus/Translate";
 import HomepageIntro from "@site/src/components/HomepageIntro";
+import TextSvgLight from "@site/static/img/rw-text-white.svg";
+import TextSvgDark from "@site/static/img/rw-text-black.svg";
 
 const ButtonGroup = styled("div")((props) => ({
   display: "flex",
@@ -36,23 +41,28 @@ const HeroBanner = styled("div")(() => ({
 function HomepageHeader() {
   const windowSize = useWindowSize();
   const { siteMetadata } = useDocusaurusContext();
+  const { isDarkTheme } = useColorMode();
 
   return (
     <HeroBanner>
       <div>
         <LogoSvg width={windowSize.height * 0.17} height={windowSize.height * 0.17} />
       </div>
-      <div style={{ fontSize: "2rem", fontWeight: "700", marginBottom: "1rem" }}>
-        <span>RisingWave</span>
+      <div >
+        {isDarkTheme ? (
+          <TextSvgLight width={windowSize.height * 0.25} height={windowSize.height * 0.1} />
+        ) : (
+          <TextSvgDark width={windowSize.height * 0.25} height={windowSize.height * 0.1}/>
+        )}
       </div>
       <ButtonGroup windoWidth={windowSize.width}>
         <Link to={`/docs/${siteMetadata.siteVersion}/intro`}>
-          <Button variant="contained" sx={{ width: 200, margin: "5px" }}>
+          <Button variant="contained" sx={{ width: 200, margin: "5px", textTransform: "none" }}>
             <Translate>Read Docs</Translate>
           </Button>
         </Link>
         <Link to={`https://playground.risingwave.dev/`}>
-          <Button variant="outlined" sx={{ width: 200, margin: "5px" }}>
+          <Button variant="outlined" sx={{ width: 200, margin: "5px", textTransform: "none" }}>
             <Translate>Try Demo</Translate>
           </Button>
         </Link>
@@ -62,8 +72,6 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
-
   // useEffect(() => {
   //   window.location.href = window.location.href + "docs/latest/intro";
   // }, []);
